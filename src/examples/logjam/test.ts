d437da7bfa0128a9c0a95ab30a7122f16ae767fa
leandroluz97 <leandrodaluz97@gmail.com>
2023-09-03 16:55:29 +0100

    added date mock

--- src/examples/logjam/test.ts
+++ src/examples/logjam/test.ts
@@ -1,3 +1,11 @@
 import { test, expect, vi } from 'vitest';
+import { log } from './log';
+import { useDispatch } from 'react-redux';
 
-test.todo('it spies on the multiply method');
+//test.todo('it spies on the multiply method');
+
+vi.mock('react-redux', (args) => {
+  return {
+    useDispatch() {},
+  };
+});
--- src/examples/time-zone/time-zone.test.tsx
+++ src/examples/time-zone/time-zone.test.tsx
@@ -1,6 +1,15 @@
-import { test, expect, vi } from 'vitest';
+import { test, expect, vi, beforeEach, afterEach } from 'vitest';
 import { render } from 'test/utilities';
 import TimeZone from '.';
+
+beforeEach(() => {
+  vi.useFakeTimers();
+  vi.setSystemTime(1679492355195);
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
 
 test('it should render successfully', () => {
   render(<TimeZone />);
